e7808e958471385f9c2dbf72bdc58556c5fe38ce
Ricardo Zamora <iyttahm@me.com>
2022-08-21 18:44:25 +0200

    update image to use ubuntu 20.04 and cuda 11.7

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-PARENT=stablebaselines/stable-baselines3
+CPU_PARENT=ubuntu:20.04
+GPU_PARENT=nvidia/cuda:11.7.1-cudnn8-runtime-ubuntu20.04
 
 TAG=habaneroforever/rl-baselines3-zoo
 VERSION=1.5.1a6
 
 if [[ ${USE_GPU} == "True" ]]; then
-  PARENT="${PARENT}:${VERSION}"
+  PARENT=${GPU_PARENT}
+  PYTORCH_DEPS="cudatoolkit==11.7"
 else
-  PARENT="${PARENT}-cpu:${VERSION}"
+  PARENT=${CPU_PARENT}
+  PYTORCH_DEPS="cpuonly"
   TAG="${TAG}-cpu"
 fi
 
-docker build --build-arg PARENT_IMAGE=${PARENT} --build-arg USE_GPU=${USE_GPU} -t ${TAG}:${VERSION} . -f docker/Dockerfile
+docker build --build-arg PARENT_IMAGE=${PARENT} --build-arg PYTORCH_DEPS=${PYTORCH_DEPS} -t ${TAG}:${VERSION} . -f docker/Dockerfile
 docker tag ${TAG}:${VERSION} ${TAG}:latest
 
 if [[ ${RELEASE} == "True" ]]; then

38fec32e3d16a219884808c2ba59a63e9e46e148
Ricardo Zamora <iyttahm@me.com>
2022-08-23 10:39:45 +0200

    parse features_extractor_class

--- utils/exp_manager.py
+++ utils/exp_manager.py
@@ -377,6 +377,13 @@ class ExperimentManager:
             if kwargs_key in hyperparams.keys() and isinstance(hyperparams[kwargs_key], str):
                 hyperparams[kwargs_key] = eval(hyperparams[kwargs_key])
 
+        ext_key = "features_extractor_class"
+        pol_key = "policy_kwargs"
+        if pol_key in hyperparams.keys() and ext_key in hyperparams[pol_key].keys():
+            if ext_key in hyperparams[pol_key].keys() and isinstance(hyperparams[pol_key][ext_key], str):
+                hyperparams[pol_key][ext_key] = get_extractor_class(hyperparams[pol_key])
+        print(hyperparams)
+
         # Delete keys so the dict can be pass to the model constructor
         if "n_envs" in hyperparams.keys():
             del hyperparams["n_envs"]
